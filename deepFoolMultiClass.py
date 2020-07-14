3c40a66b25419761799b425e4a45bf9023f8644a
Domastos <isavchenko18@gmail.com>
2020-07-14 22:33:59 +0200

    cifar10 model trained

--- deepFoolMultiClass.py
+++ deepFoolMultiClass.py
@@ -1,9 +1,19 @@
-import numpy
+import numpy as np
 import tensorflow as tf
 from copy import deepcopy
 
 
 def deepFoolMultiClass(image, classifier):
+
+    shape=(28, 28, 1)
+
+    image_array = np.array(image)
+    image_normalized = np.reshape(tf.cast(image_array / 255.0 - 0.5, tf.float32),\
+                                   shape)
+    image_f = classifier(image_normalized[tf.newaxis, ...]).flatten()
+
+
+    
     return 0
 
 
