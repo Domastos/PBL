0d17b25b813cbc51035cb3cd61d88a10f8535e8a
Domastos <isavchenko18@gmail.com>
2020-07-15 08:37:32 +0200

    deepfool implementation started

--- deepFoolMultiClass.py
+++ deepFoolMultiClass.py
@@ -1,19 +1,39 @@
 import numpy as np
 import tensorflow as tf
 from copy import deepcopy
+from keras.preprocessing.image import load_img, img_to_array
+from keras.models import load_model
 
-
-def deepFoolMultiClass(image, classifier):
-
-    shape=(28, 28, 1)
-
-    image_array = np.array(image)
-    image_normalized = np.reshape(tf.cast(image_array / 255.0 - 0.5, tf.float32),\
-                                   shape)
-    image_f = classifier(image_normalized[tf.newaxis, ...]).flatten()
-
-
-    
+def loss():
     return 0
 
+
+def deepFoolMultiClass(imageName, modelName, numOfLabels):
+
+    #load_image
+    img = load_img(imageName, target_size=(32, 32))
+    img = img_to_array(img)
+    img = img.reshape(1, 32, 32, 3)
+    img = img.astype('float32')/ 255.0
+
+
+    I = (np.array(img)).flatten().argsort()[::-1]
+    I = I[0:numOfLabels]
+
+    #f()
+    model = load_model(modelName)
+
+    #f(X0)
+    initLable = model.predict_classes(img)[0]
+          
+    #f(x1)
+    Label = initLable 
+
+    #when ki = k
+    # while Label == initLable:
+
+        #for classes that are not image                   
+        # for i in range(1, numOfLabels):
+
+    return img
 
